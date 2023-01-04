210f7bd35b8d2f17d519729c0a21c197c53ebffc
Shion <smatsumoto97@gmail.com>
2023-01-04 22:20:17 +0900

    jk to escape

--- lua/shion/remap.lua
+++ lua/shion/remap.lua
@@ -1,5 +1,8 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Press jk fast to esc
+vim.keymap.set("i", "jk", "<Esc>")
+
 -- Move blocks of code
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
