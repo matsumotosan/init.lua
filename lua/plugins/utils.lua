506e84fc437c57c57ec610b483be7a77bd52f38c
Shion <smatsumoto97@gmail.com>
2023-05-08 08:43:11 +0900

    change fidget icon

---
+++ lua/plugins/utils.lua
@@ -0,0 +1,13 @@
+return {
+  -- Useful plugin to show you pending keybinds
+  { 'folke/which-key.nvim' , opts = {} },
+
+  { 'windwp/nvim-autopairs', opts = {} },
+  { 'kyazdani42/nvim-web-devicons' , opts = {} },
+
+  -- Git
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  'tpope/vim-sleuth',
+}
